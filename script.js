4edb330d02235cba0bc4e26a1f19dc925fb9a905
tayehassan <57362295+tayehassan@users.noreply.github.com>
2022-05-18 15:14:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
     harmburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+ 
+	togglePassword.addEventListener('click', function (e) {
+	// toggle the type attribute
+	const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
